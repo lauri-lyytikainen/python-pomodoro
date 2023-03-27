ebb11cc90aeab230bd33bb0d7cf3f5342f90bbe0
Lauri Lyytikäinen <lauri.lyytikainen1@gmail.com>
2023-03-27 16:56:17 +0300

    initial commit

---
+++ pomodoro.py
@@ -0,0 +1,169 @@
+import tkinter.ttk as ttk
+import tkinter as tk
+import ttkthemes as ThemedStyles
+import time
+
+class Pomodoro(ttk.Frame):
+
+    onTop = False
+    timerSeconds = 0        
+    timeTitleText = ""
+    timerRunning = False
+    timerTask = None
+    notifyOnComplete = False
+    style = None
+
+    darkMode = False
+
+    def startTimer(self, minutes):
+        self.timerSeconds = int(minutes) * 60
+        self.timeTitle.config(text=self.timeTitleText)
+        self.timeDisplay.config(text="00:00")
+        self.updateTimer()
+
+    def updateTimer(self):
+        if not self.timerRunning:
+            # cancel the timer task
+            self.parent.after_cancel(self.timerTask)
+            return
+        if self.timerSeconds > 0:
+            self.timerSeconds -= 1
+            self.timeDisplay.config(text=time.strftime("%M:%S", time.gmtime(self.timerSeconds)))
+            self.timerTask = self.parent.after(1000, self.updateTimer)
+        else:
+            if self.notifyOnComplete:
+                # Notify the user that the time is up
+                self.parent.bell()
+                # Make the window always on top
+                self.parent.call('wm', 'attributes', '.', '-topmost', '1')
+                # Make the window always on top as set by the user
+                self.parent.after(1000, lambda: self.parent.call('wm', 'attributes', '.', '-topmost', '1' if self.onTop else '0'))
+                # Change the background color back to normal
+                self.parent.after(1000, lambda: self.parent.config(bg="SystemButtonFace"))
+
+
+            if self.timeTitleText == "Productivity Phase":
+                self.timeTitleText = "Break Phase"
+                seconds = 0
+                try:
+                    seconds = int(self.breakTime.get())
+                except ValueError:
+                    seconds = 25
+                self.startTimer(seconds)
+            else:
+                self.timeTitleText = "Productivity Phase"
+                minutes = 0
+                try:
+                    minutes = int(self.productivityTime.get())
+                except ValueError:
+                    minutes = 25
+                self.startTimer(minutes)
+
+    def start(self):
+        if self.timerRunning:
+            return
+        self.timeTitleText = "Productivity Phase"
+        self.timerRunning = True
+        minutes = 0
+        try:
+            minutes = int(self.productivityTime.get())
+        except ValueError:
+            minutes = 25
+        self.startTimer(minutes)
+
+    def stop(self):
+        self.timerRunning = False
+        self.timeTitle.config(text="Productivity Phase")
+        self.timeDisplay.config(text="00:00")
+    
+    def toggleAlwaysOnTop(self):
+        self.onTop = not self.onTop
+        self.parent.call('wm', 'attributes', '.', '-topmost', '1' if self.onTop else '0')
+
+    def toggleNotifyOnComplete(self):
+        self.notifyOnComplete = not self.notifyOnComplete
+
+    def toggleDarkMode(self):
+        self.darkMode = not self.darkMode
+        if self.darkMode:
+            self.style.set_theme("equilux")
+        else:
+            self.style.set_theme("arc")
+
+    def __init__(self, parent, *args, **kwargs):
+        ttk.Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
+        
+        self.parent.title("Pomodoro Timer")
+        self.parent.geometry("350x250")
+        self.parent.resizable(0,0)
+
+        self.style = ThemedStyles.ThemedStyle(self.parent)
+        self.style.set_theme("arc")
+
+        self.mainFrame = ttk.Frame(self.parent)
+
+        self.label = ttk.Label(self.mainFrame, text="Pomodoro Timer", font=("Arial", 20))
+        self.label.pack()
+
+        # Add a big space between the label and the buttons
+        self.spacer1 = ttk.Label(self.mainFrame, text="")
+        self.spacer1.pack()
+
+
+        self.timeFrame = ttk.Frame(self.mainFrame)
+
+        # Create a input for minutes for productivity and break
+        self.productivityLabel = ttk.Label(self.timeFrame, text="Productivity Time")
+        self.productivityLabel.pack(side="left")
+        self.productivityTime = ttk.Entry(self.timeFrame, width=5, validate="key", validatecommand=(self.parent.register(lambda P: P.isdigit() and int(P) > 0 and int(P) <= 60 or P == ""), '%P'))
+        self.productivityTime.insert(0, "25")
+        self.productivityTime.pack(side="left")
+
+        self.breakLabel = ttk.Label(self.timeFrame, text="Break Time")
+        self.breakLabel.pack(side="left")
+
+        self.breakTime = ttk.Entry(self.timeFrame, width=5, validate="key", validatecommand=(self.parent.register(lambda P: P.isdigit() and int(P) > 0 and int(P) <= 60 or P == ""), '%P'))
+        self.breakTime.insert(0, "5")
+        self.breakTime.pack(side="left")
+        
+        self.timeFrame.pack()
+
+        self.spacer2 = ttk.Label(self.mainFrame, text="")
+        self.spacer2.pack()
+
+        self.timeTitle = ttk.Label(self.mainFrame, text="Productivity Phase", font=("Arial", 20))
+        self.timeTitle.pack()
+
+        self.timeDisplay = ttk.Label(self.mainFrame, text="00:00", font=("Arial", 20))
+        self.timeDisplay.pack()
+
+        self.checkBoxFrame = ttk.Frame(self.mainFrame)
+        self.alwaysOnTopCheckbox = ttk.Checkbutton(self.checkBoxFrame, text="Always on top", command=self.toggleAlwaysOnTop)
+        self.alwaysOnTopCheckbox.state(['!alternate'])
+        self.alwaysOnTopCheckbox.pack(side="left")
+        self.notifyOnCompleteCheckbox = ttk.Checkbutton(self.checkBoxFrame, text="Notify on complete", command=self.toggleNotifyOnComplete)
+        self.notifyOnCompleteCheckbox.state(['!alternate'])
+        self.notifyOnCompleteCheckbox.pack(side="left")
+
+        self.checkBoxFrame.pack()
+
+        self.darkModeCheckbox = ttk.Checkbutton(self.mainFrame, text="Dark Mode", command=self.toggleDarkMode)
+        self.darkModeCheckbox.state(['!alternate'])
+        self.darkModeCheckbox.pack()
+
+        self.buttonFrame = ttk.Frame(self.mainFrame)
+        self.startButton = ttk.Button(self.buttonFrame, text="Start", command=self.start)
+        self.startButton.pack(side="left")
+        self.stopButton = ttk.Button(self.buttonFrame, text="Stop", command=self.stop)
+        self.stopButton.pack(side="left")
+
+        self.buttonFrame.pack()
+        
+        self.mainFrame.pack(fill="both", expand=True)
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    Pomodoro(root).pack(side="top", fill="both", expand=True)
+
+    root.mainloop()
